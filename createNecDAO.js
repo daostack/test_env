dc695a2c569cbf2f6649ee8bceb9a56d4bfabace
Jelle <jellegerbrandy@gmail.com>
2020-01-10 15:19:45 +0100

    add reputationfor tokens rc.34 (#23)

--- createNecDAO.js
+++ createNecDAO.js
@@ -5,13 +5,11 @@ const { migrateDAO } = require('@daostack/migration')
 const { VERSION } = require('./deployEthereum')
 
 async function createNectarDAO(options) {
-
   const arcVersion = '0.0.1-rc.32'
   // get the params suitable for v32
   options.params = require('./nectardao-params-v32.json')
   options.arcVersion = arcVersion
   const result = await migrateDAO(options)
-
   return result
 }
 
