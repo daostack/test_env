c7cda3f0ae21d3687ed93f35a9b678367e2939a7
dOrgJelli <jelli@dorg.tech>
2020-04-22 15:20:29 -0400

    fixed ipfs version

--- release.sh
+++ release.sh
@@ -27,7 +27,7 @@ image_version=$package_version
 echo "Starting fresh docker containers..."
 set -x # echo on
 docker-compose down -v
-docker-compose up -d
+docker-compose up --force-recreate --build -d
 
 set +x
 echo "waiting for ganache to start"
