2d4e7dea45fac65ca11fc0d92a825d8fb0d11b59
benk10 <ben.kaufman10@gmail.com>
2020-08-05 10:58:12 +0300

    Revert "fix?"
    This reverts commit d3b4776ca8b145c781f90fdfb158ca6ee71e9dd4.

--- release.sh
+++ release.sh
@@ -49,11 +49,10 @@ docker-compose exec  ganache cat migration.json > migration.json
 
 npm run deployEthereum
 
-# echo "waiting for graph-node to start"
-# set +x
-# while [[ ! "$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8000)" =~ ^(200|302)$ ]]; do sleep 5; done
-# set -x
-sleep 60
+echo "waiting for graph-node to start"
+set +x
+while [[ ! "$(curl -s -o /dev/null -w ''%{http_code}'' 127.0.0.1:8000)" =~ ^(200|302)$ ]]; do sleep 5; done
+set -x
 
 # echo pwd
 
